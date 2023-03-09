2268684859f1f2e285636b5ceb164e4894e3d3bd
lozananastasia <lozananastasia20@stud.ase.ro>
2023-03-09 11:51:41 +0200

    Applied SOLID principles

---
+++ src/ro/ase/acs/classes/AngajatTC.java
@@ -0,0 +1,21 @@
+package ro.ase.acs.classes;
+
+import ro.ase.acs.interfaces.TableCreator;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class AngajatTC implements TableCreator {
+    @Override
+    public void createTable(Connection connection) throws Exception {
+        String sqlDrop = "DROP TABLE IF EXISTS employees";
+        String sqlCreate = "CREATE TABLE employees(id INTEGER PRIMARY KEY,"
+                + "name TEXT, address TEXT, salary REAL)";
+
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(sqlDrop);
+        statement.executeUpdate(sqlCreate);
+        statement.close();
+        connection.commit();
+    }
+}
